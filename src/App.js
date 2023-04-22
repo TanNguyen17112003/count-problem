120940f3a07320b39ad93fbb078b3079ccb60a41
your_name <your_address_email@example.com>
2023-04-23 00:01:49 +0700

    first commit count problem using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
+import {useReducer} from 'react';
 import './App.css';
-
+const initCount = { count: 0 }
+function reducer(state, action) {
+  if (action.type == "increase") {
+    return { count: state.count + 1 };
+  }
+  else if (action.type == "decrease") {
+    return { count: state.count - 1 };
+  }
+  else if (action.type == "reset") {
+    return { count: state.count = 0 };
+  }
+  else {
+    return { count: state.count };
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initCount);
+  const handleChange = (count, kind) => {
+    if (kind == 1) {
+      dispatch({
+        type: "increase"
+      })
+    }
+    else if (kind == 2) {
+      dispatch({
+        type: "decrease"
+      })
+    }
+    else if (kind == 3) {
+      dispatch({
+        type: "reset"
+      })
+    } 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>This project for testing using useReducer by using count problem</h2>
+      <span>Count: {state.count}</span>
+      <br />
+      <button onClick={() => handleChange(state, 1)}>Increase</button>
+      <button onClick={() => handleChange(state, 2)}>Decrease</button>
+      <button onClick={() => handleChange(state, 3)}>Reset</button>
     </div>
   );
 }
